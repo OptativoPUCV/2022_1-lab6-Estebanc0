54dfca72c991780f61994cf866a0d0d075537d8b
Your Name <you@example.com>
2022-05-15 21:06:17 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -106,7 +106,7 @@ List* get_adj_nodes(Node* n){
 int is_final(Node* n){
   for(int i=0 ;i<9;i++){
     for (int j =0;j<9;j++){
-      printf("[%i,%i] = %i\n",i,j,n->sudo[i][j]);
+      //printf("[%i,%i] = %i\n",i,j,n->sudo[i][j]);
       if(n->sudo[i][j]==0) return 0;
     }
   }
